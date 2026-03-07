a68f32fa599cb19591a5b0fabf079e172f59dd64
Sunghyun Park <sunghyunp75@gmail.com>
2026-03-08 02:18:39 +0900

    'my'

--- app/(tabs)/my/index.tsx
+++ app/(tabs)/my/index.tsx
@@ -1,6 +1,11 @@
 import { colors } from '@/constants';
+import { useGetMineRequestList } from '@/hooks/queries/join/use-get-mine-join-request.data';
 import { useAuthActions, useUserSession } from '@/store/useAuthStore';
-import { router } from 'expo-router';
+import { JoinRequest } from '@/types';
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useQueryClient } from '@tanstack/react-query';
+import { router, useLocalSearchParams } from 'expo-router';
 import { Alert, Image, Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,6 +13,21 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function MyScreen() {
   const user = useUserSession();
   const { setLogOut } = useAuthActions();
+  const queryClient = useQueryClient();
+
+  const { data: mineRequestGroup } = useGetMineRequestList(user?.token || '');
+  const { groupId } = useLocalSearchParams<{ groupId: string }>();
+
+  const handleEditNickname = () => {
+    router.push({
+      pathname: '/auth/nicknameSetting',
+      params: {
+        mode: 'edit',
+        prevRunnerNickname: user?.runnerName,
+        prevRunnerImageLink: user?.runnerImageLink,
+      },
+    });
+  };
 
   const handleLogOut = () => {
     Alert.alert('로그아웃', '로그아웃 하시겠습니까?', [
@@ -18,18 +38,35 @@ export default function MyScreen() {
       {
         text: '확인',
         style: 'destructive',
-        onPress: () => {
-          setLogOut();
+        onPress: async () => {
+          try {
+            await GoogleSignin.signOut();
 
-          router.replace('/auth/signIn');
+            await auth().signOut();
+
+            queryClient.clear();
+
+            router.replace('/auth/signIn');
+          } catch (error) {
+            console.error('로그아웃 에러', error);
+            setLogOut();
+            router.replace('/auth/signIn');
+          }
         },
       },
     ]);
   };
-  console.log('현재 세션 유저 정보:', user?.runnerId);
-  console.log(JSON.stringify(user, null, 2));
 
   const defaultImage = require('@/assets/images/default-avatar.jpg');
+
+  const getProfileSource = () => {
+    const link = user?.runnerImageLink;
+
+    if (!link) {
+      return defaultImage;
+    }
+    return { uri: link };
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -39,22 +76,39 @@ export default function MyScreen() {
 
       <View style={styles.user}>
         <View style={styles.iconWrapper}>
-          <Image
-            source={
-              user?.runnerImageLink
-                ? { uri: user.runnerImageLink }
-                : defaultImage
-            }
-            style={styles.profileImage}
-          />
+          <Image source={getProfileSource()} style={styles.profileImage} />
         </View>
         <View style={styles.textWrapper}>
           <Text style={{ fontFamily: 'pretendard400', fontSize: 16 }}>
             {user?.runnerName}
           </Text>
-          <Text style={{ fontFamily: 'pretendard400', fontSize: 14 }}>
-            공부 열심히 하자!
-          </Text>
+          <Pressable onPress={handleEditNickname}>
+            <Text>닉네임 설정</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View>
+        <Text>그룹 신청 현황</Text>
+
+        <View>
+          {mineRequestGroup && mineRequestGroup.length > 0 ? (
+            mineRequestGroup.map((item: JoinRequest) => {
+              const group = item.groupId || item;
+
+              return (
+                <View key={item.groupId}>
+                  <View>
+                    <Text>{item.groupName}</Text>
+                  </View>
+                  <View>
+                    <Text>대기 중</Text>
+                  </View>
+                </View>
+              );
+            })
+          ) : (
+            <Text>신청한 그룹이 없습니다.</Text>
+          )}
         </View>
       </View>
       <View style={styles.setting_space}>
