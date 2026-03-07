d6942c54f6dc94a5baa96b8ea0ea99a207eae3a0
Sunghyun Park <sunghyunp75@gmail.com>
2026-03-08 02:25:45 +0900

    query_key

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,39 @@ export const QUERY_KEYS = {
     list: ['group', 'list'],
     groupList: (groupId: string) => ['group', 'list', groupId],
     runnersList: (groupId: string) => ['group', 'runners', groupId],
-    joinedGroups: (runnerId: string) => ['group', 'joined', runnerId],
+    joinedGroups: (token: string, runnerId?: string) => [
+      'group',
+      'list',
+      'joined',
+      token,
+      runnerId,
+    ],
+    mineGroups: (token: string) => ['group', 'mine', token],
+    searchGroups: (token: string, groupName: string) => [
+      'group',
+      'search',
+      token,
+      groupName,
+    ],
+    updateLeader: (
+      token: string,
+      groupId: string,
+      newLeaderRunnerId: string,
+    ) => ['group', 'leader', token, groupId, newLeaderRunnerId],
+  },
+  auth: {
+    existRunner: (token?: string) => ['auth', 'existRunner', token],
+    runnerInfo: () => ['runner', 'runnerInfo'],
+  },
+  request: {
+    all: ['request'],
+    list: ['request', 'list'],
+    joinRequest: (joinRequestId: string, groupId?: string) => [
+      'request',
+      'list',
+      joinRequestId,
+      groupId,
+    ],
+    mineRequestList: (token: string) => ['request', 'mine', token],
   },
 };
