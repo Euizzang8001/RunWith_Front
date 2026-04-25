9658ff89d0dc384c89b6fb01a553b57ed1bb87d3
Sunghyun Park <sunghyunp75@gmail.com>
2026-04-25 15:20:36 +0900

    [feat] 리프레시 토큰 자동 갱신 추가

---
+++ api/authfetch.ts
@@ -0,0 +1,27 @@
+import { useAuthStore } from '@/store/useAuthStore';
+import { getAuth } from '@react-native-firebase/auth';
+
+// 리프레쉬 토큰 갱신
+
+export const authFetch = async (url: string, options: RequestInit = {}) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const headers = new Headers(options.headers);
+
+  if (user) {
+    const token = await user.getIdToken(true);
+    headers.set('Authorization', `Bearer ${token}`);
+    if (!(options.body instanceof FormData)) {
+      headers.set('Content-Type', 'application/json');
+    }
+  }
+
+  const response = await fetch(url, { ...options, headers });
+
+  if (response.status === 401) {
+    useAuthStore.getState().actions.setLogOut();
+    throw new Error('인증 세션 만료');
+  }
+
+  return response;
+};
